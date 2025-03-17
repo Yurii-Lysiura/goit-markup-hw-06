bedb951acafbdd79bbd65c78671cc0e74adffd2e
Yurii Lysiura <taitgm@gmail.com>
2025-03-17 17:31:25 +0200

    hw6-end

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
+    // Додати атрибут data-menu-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-menu-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
+    // Додати атрибут data-menu-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-menu-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
+    // Додати атрибут data-menu на бекдроп модалки
     modal: document.querySelector('[data-menu]'),
   };
 
